8523a0156c5efe9e578c2c20afecb5a8ad0495e9
Chris Roche <croche314@gmail.com>
2017-06-22 08:31:45 -0500

    mongo production db

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 var fs = require('fs');
 var path = require('path');
 
-mongoose.connect('mongodb://localhost/eStore_DB');
+// mongoose.connect('mongodb://localhost/eStore_DB');
+mongoose.connect('mongodb://heroku_22qxd1wq');
 var models_path = path.join(__dirname, "../models");
 
 fs.readdirSync(models_path).forEach(function(file) {
