339f6f38d6982b0c134499c2b841f4a628eb6a51
Chris Roche <croche314@gmail.com>
2017-06-22 08:35:15 -0500

    mongoose change to production

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var path = require('path');
 
 // mongoose.connect('mongodb://localhost/eStore_DB');
-mongoose.connect('mongodb://heroku_22qxd1wq');
+mongoose.connect('process.env.ds133922.mlab.com:33922/heroku_22qxd1wq');
 var models_path = path.join(__dirname, "../models");
 
 fs.readdirSync(models_path).forEach(function(file) {
