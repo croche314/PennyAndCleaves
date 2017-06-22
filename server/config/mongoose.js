c78302df5c3175b42c134c1ec47a575820218f20
Chris Roche <croche314@gmail.com>
2017-06-22 08:39:58 -0500

    fixed mongodb connection uri

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var path = require('path');
 
 // mongoose.connect('mongodb://localhost/eStore_DB');
-mongoose.connect('process.env.ds133922.mlab.com:33922/heroku_22qxd1wq');
+mongoose.connect('mongodb://heroku_22qxd1wq:2huvv9vv7anetcn5l5q8ov1mck@ds133922.mlab.com:33922/heroku_22qxd1wq');
 var models_path = path.join(__dirname, "../models");
 
 fs.readdirSync(models_path).forEach(function(file) {
